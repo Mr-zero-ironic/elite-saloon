a207a42a02db08f441ef23b392f7fc59f3c134c5
mrzero <mrz114768@gmail.com>
2025-12-16 21:02:58 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ document.querySelectorAll(".nav-links a").forEach((link) => {
     navLinks.classList.remove("animated");
   });
 });
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 40,
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+//whatsapp button functionality
+const whatsappBtn = document.querySelector("#wa-btn");
+whatsappBtn.addEventListener("click", () => {
+  window.open("https://wa.me/923233341296", "_blank");
+});
+
+//theme toggle functionality
+const themeBtn = document.querySelector("#theme-btn");
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  themeBtn.src = document.body.classList.contains("light-theme")
+    ? "assets/img/moon-fill.png"
+    : "assets/img/sun-fill.png";
+});
